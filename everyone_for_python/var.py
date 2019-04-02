58042b0e1fb7f02c1cbeaa80590489bfbad5b364
Tomohisa Uchida <uchida@uddystyle.com>
2019-04-03 04:42:59 +0900

    Finished variables

---
+++ everyone_for_python/var.py
@@ -0,0 +1,14 @@
+pi = 3.141592
+diameter = 12756.274
+total = pi * diameter
+print(total)
+
+cal_per_1kg = 7200
+cal_per_1minjog = 7.76
+min_to_lose1kg = cal_per_1kg / cal_per_1minjog
+
+print(min_to_lose1kg)
+
+
+hours_to_lose1kg = min_to_lose1kg / 60
+print(hours_to_lose1kg)
