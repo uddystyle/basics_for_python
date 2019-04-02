b3da2e20f3ae3334f6d16c0713e3060ca39d2d4e
Tomohisa Uchida <uchida@uddystyle.com>
2019-04-03 04:57:41 +0900

    Finished string.py

---
+++ everyone_for_python/string.py
@@ -0,0 +1,19 @@
+a_lylic = "でもね私のエネルギーは"
+a_lylic = a_lylic + "すでにインフィニティだよ。"
+print(a_lylic)
+
+# 複合演算子
+lylic2 = "ずっと笑顔ばかりを選んで"
+lylic2 += "泣き顔を見せるのを迷ってた"
+print(lylic2)
+
+# 型を揃える
+# day = 24
+# date = day + "日"
+# print(date) 違うデータ型を足し算するのでエラーになる
+
+# 文字列と数値の変換（型変換）
+day = 24
+str_day = str(day)
+date = str_day + "日"
+print(date)
